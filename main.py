aeaf8c699b270a350c8664d1d158beb166627ce8
MahnFrammen <71048879+MahnFrammen@users.noreply.github.com>
2022-11-28 21:17:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,197 @@
+# Zach Pedersen, Rylan Casanova
+# This is our work!
+# CST-305
+# Prof. Citro
+
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import time
+import math
+
+# Our ODEs
+def dudx1(u, x):
+    return [u[1], 2 * x * u[1] - x ** 2 * u[0]]
+
+def dudx2(u, x):
+    return [u[1], (x - 2) * u[1] - 2 * u[0]]
+
+
+# Computes y's using Taylor Polynomial
+# Taylor polynomial of degree 4
+
+def taylor1(x):
+    return 1 - x - (1 / 3) * x ** 3 - (1 / 12) * x ** 4
+
+
+# Taylor polynomial of degree 2
+
+def taylor2(x):
+    return 6 + (x - 3) - (11 / 2) * (x - 3) ** 2
+
+
+def dudx3(u, x):
+    return [u[1], 0 * u[1] - (1 / (x ** 2 + 4)) * u[0] + x / (x ** 2 + 4)]
+
+
+def computer(u, x):
+    return [u[1], -x * u[1] - x ** 2 * u[0] + x ** 3]
+
+
+# Records start time (Odeint1)
+start = time.time()
+
+# number of points
+r = 101
+# start conditions
+x = np.linspace(-5, 5, r)
+u = [1, -1]
+
+# Calculates with Odeint
+uy = odeint(dudx1, u, x)
+y = uy[:, 0]
+
+# Calculates time for Odeint1
+t = time.time() - start
+print("Odeint1 operation took ", t, " seconds.")
+
+# Plot results (Odeint1)
+plt.title('Odeint1')
+plt.plot(x, y, label="Odeint1")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+# Records start time (Taylor1)
+start = time.time()
+
+# number of points
+r = 101
+# start conditions
+x3 = np.linspace(-5, 5, r)
+
+# Calculates with Taylor Polynomial
+y3 = taylor1(x3)
+
+# Calculates time for Taylor1
+t = time.time() - start
+print("Taylor1 operation took ", t, " seconds.")
+
+# Plot results (Taylor1)
+plt.title('Taylor1')
+plt.plot(x3, y3, label="Taylor1", linestyle=":")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+# Show Odeint and Taylor together
+plt.plot(x, y, label="Odeint1")
+plt.plot(x3, y3, label="Taylor1", linestyle=":")
+plt.legend()
+plt.show()
+
+# Records start time (Odeint2)
+start = time.time()
+
+# number of points
+r = 101
+# start conditions
+x2 = np.linspace(0, 6, r)
+u2 = [6, 1]
+
+# Calculates with Odeint
+uy2 = odeint(dudx2, u2, x2)
+y2 = uy2[:, 0]
+
+# Calculates time for Odeint2
+t = time.time() - start
+print("Odeint2 operation took ", t, " seconds.")
+
+# Plot results (Odeint2)
+plt.title('Odeint2')
+plt.plot(x2, y2, label="Odeint2")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+# Records start time (Taylor2)
+start = time.time()
+
+# number of points
+r = 101
+# start conditions
+x4 = np.linspace(0, 6, r)
+
+# Calculates with Taylor Polynomial
+y4 = taylor2(x4)
+
+# Calculates time for Taylor2
+t = time.time() - start
+print("Taylor2 operation took ", t, " seconds.")
+
+# Plot results (Taylor2)
+plt.title('Taylor2')
+plt.plot(x4, y4, label="Taylor2", linestyle=":")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+# Show Odeint and Taylor together
+plt.plot(x2, y2, label="Odeint2")
+plt.plot(x4, y4, label="Taylor2", linestyle=":")
+plt.legend()
+plt.show()
+
+# Records start time (Odeint3)
+start = time.time()
+
+# number of points
+r = 101
+# start conditions
+x3 = np.linspace(0, 10, r)
+u3 = [0, 0]
+
+# Calculates with Odeint
+uy3 = odeint(dudx3, u3, x3)
+y3 = uy3[:, 0]
+
+# Calculates time for Odeint2
+t = time.time() - start
+print("Odeint3 operation took ", t, " seconds.")
+
+# Plot results (Odeint2)
+plt.title('Odeint3')
+plt.plot(x3, y3, label="Odeint3")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+# Records start time (cpu)
+start = time.time()
+
+# number of points
+r = 101
+# start conditions
+xc = np.linspace(0, 30, r)
+uc = [0, 0]
+
+# Calculates with Odeint
+uyc = odeint(computer, uc, xc)
+yc = uyc[:, 0]
+
+# Calculates time for cpu
+t = time.time() - start
+print("cpu operation took ", t, " seconds.")
+
+# Plot results (cpu)
+plt.title('computer')
+plt.plot(xc, yc, label="computer")
+plt.xlabel('cost')
+plt.ylabel('best performance')
+plt.legend()
+plt.show()
